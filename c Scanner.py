d28fca97d1eb12868869117f3c5f0755f19d2e00
KareemAdel10 <119543678+KareemAdel10@users.noreply.github.com>
2023-11-26 21:28:58 +0200

    Update c Scanner.py

--- c Scanner.py
+++ c Scanner.py
@@ -1,68 +1,41 @@
-def is_keyword(word):
-    keywords = ["auto", "break", "case", "char", "const", "continue", "default",
-                "do", "double", "else", "enum", "extern", "float", "for", "goto",
-                "if", "int", "long", "register", "return", "short", "signed", "sizeof",
-                "static", "struct", "switch", "typedef", "union", "unsigned", "void",
-                "volatile", "while"]
-    return word in keywords
-
-def is_special_character(char):
-    operators = set("`~@!$#^&()[]{}_|\\;:'\",.?")
-    return char in operators
-
-def is_operator(char):
-    return char in set(["+", "-", "/", "*", "%", "++", "--", "=", "<", ">", "<>", "<=", ">="])
-
-def is_numeric_literal(token):
-    try:
-        float(token)
-        return True
-    except ValueError:
-        return False
+import re
 
 def scan_tokens(input_code):
     tokens = []
-    current_token = ""
 
-    for char in input_code:
-        if char.isalnum() or char == '.' or char == 'e' or char == 'E' or char == '-' or char == '+':
-            current_token += char
+    # Define regular expressions for different token types
+    keyword_pattern = re.compile(r"\b(auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\b")
+    special_character_pattern = re.compile(r"[`~@!$#^&()[\]{}_|\\;:',.?]")
+    operator_pattern = re.compile(r"\+\+|--|[-+*/%=<>&|\[\]=!]")
+    numeric_literal_pattern = re.compile(r"[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?")
+    identifier_pattern = re.compile(r"[a-zA-Z_]\w*")
+
+    # Combine all patterns into a list of tuples
+    patterns = [
+        ("Keyword", keyword_pattern),
+        ("SpecialCharacter", special_character_pattern),
+        ("Operator", operator_pattern),
+        ("NumericConstant", numeric_literal_pattern),
+        ("Identifier", identifier_pattern),
+    ]
+
+    while input_code:
+        # Iterate through each pattern and check if it matches the beginning of the input
+        for token_type, pattern in patterns:
+            match = pattern.match(input_code)
+            if match:
+                tokens.append((token_type, match.group()))
+                input_code = input_code[len(match.group()):].lstrip()
+                break
         else:
-            if current_token:
-                if is_keyword(current_token):
-                    tokens.append(("Keyword", current_token))
-                elif is_numeric_literal(current_token):
-                    tokens.append(("NumericLiteral", current_token))
-                else:
-                    tokens.append(("Identifier", current_token))
-                current_token = ""
-
-            if char.isspace() or char in ["\n", "\r", "\t"]:
-                continue
-
-            current_token = char
-
-            if is_special_character(current_token):
-                tokens.append(("SpecialCharacter", current_token))
-                current_token = ""
-                
-            if is_operator(current_token):
-                tokens.append(("Operator", current_token))
-                current_token = ""
-
-    # Process the last token
-    if current_token:
-        if is_keyword(current_token):
-            tokens.append(("Keyword", current_token))
-        elif is_numeric_literal(current_token):
-            tokens.append(("NumericLiteral", current_token))
-        else:
-            tokens.append(("Identifier", current_token))
+            # If none of the patterns match, there's an unrecognized character
+            tokens.append(("Unrecognized", input_code[0]))
+            input_code = input_code[1:]
 
     return tokens
 
 # Example input code
-input_code = "while ( 1.3e-2 if )"
+input_code = "for (i = 1; i = -5.1e3; i = i + 1) fun1 (a)"
 
 # Call the scanner function
 found_tokens = scan_tokens(input_code)
@@ -70,7 +43,5 @@ found_tokens = scan_tokens(input_code)
 # Print the found tokens
 for token_type, token_value in found_tokens:
     print(f"{token_type}: {token_value}")
-
-
 
 
